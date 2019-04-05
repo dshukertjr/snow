f3fa58e6521d66311fbd7afe3c1df0695fe43569
dshukertjr <dshukertjr@gmail.com>
2019-04-05 18:25:33 +0900

    added bunch of stuff

--- lib/copy.dart
+++ lib/copy.dart
@@ -11,9 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Snow的なやつ',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: MyHomePage(),
     );
   }
--- lib/start.dart
+++ lib/start.dart
@@ -26,8 +26,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
-    );
+    return Container();
   }
 }
